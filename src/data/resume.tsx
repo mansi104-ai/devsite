9647cbe6dc8c2fb8312be8201245cd4dc6974dd4
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2024-08-10 23:26:35 +0530

    changed intro

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,28 +2,32 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Mansi",
+  initials: "MK",
+  url: "https://mansikalra.vercel.app/",
+  location: "Haryana, India",
+  locationLink: "https://www.google.com/maps/place/India",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "I am an MLOps developer with a strong foundation in machine learning and neural networks, and an unyielding passion for the ever-evolving world of AI and technology.",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+    " My proficiency in these areas has allowed me to excel in various projects that span data analysis, computer vision, Flutter app development, and artificial intelligence",
   avatarUrl: "/me.png",
   skills: [
-    "React",
-    "Next.js",
-    "Typescript",
-    "Node.js",
     "Python",
-    "Go",
-    "Postgres",
-    "Docker",
-    "Kubernetes",
-    "Java",
+    "TensorFlow",
+    "Neural Networks",
+    "Arduino",
+    "Scikit-Learn",
+    "OpenCV",
+    "NLP",
+    "Transformers",
+    "SQL",
     "C++",
+    "OOPs",
+    "Computer Networks",
+    "Linux",
+    "Crytography",
+ 
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
